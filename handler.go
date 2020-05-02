73bda30febd028b632d3280259040adb30a50abc
Sudipta Pandit <realsdx@protonmail.com>
2020-05-03 01:21:09 +0530

    [UPDATE] handler with Adress space option

--- handler.go
+++ handler.go
@@ -5,6 +5,7 @@ import "flag"
 // Config struct for flags
 type Config struct {
 	CPU    int
+	AS     int
 	Memory int
 	Nproc  int
 	Fsize  int
@@ -19,7 +20,8 @@ func HandleFlags() *Config {
 	conf := new(Config) // create a new pointer to Config struct
 
 	flag.IntVar(&conf.CPU, "cpu", 5, "CPU time in seconds")
-	flag.IntVar(&conf.Memory, "mem", 32768, "Memory in KBytes")
+	flag.IntVar(&conf.AS, "addrSpcae", 65536, "Adress Space Memory in KBytes")
+	flag.IntVar(&conf.Memory, "mem", 32768, "Program Memory in KBytes")
 	flag.IntVar(&conf.Nproc, "nproc", 10, "Number of processes the program can create")
 	flag.IntVar(&conf.Fsize, "fsize", 8192, "Output file size in Kbytes")
 	flag.IntVar(&conf.Stack, "stack", 8192, "Stack size in Kbytes")
@@ -30,6 +32,7 @@ func HandleFlags() *Config {
 
 	// TODO: Check String Flags for errors
 	conf.Memory *= 1024 // convert Bytes to Kbytes
+	conf.AS *= 1024
 	conf.Fsize *= 1024
 	conf.Stack *= 1024
 
