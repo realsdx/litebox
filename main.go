e34cda70302e430519fef03ff0e8753af9f6576c
Sudipta Pandit <realsdx@protonmail.com>
2020-05-03 19:15:49 +0530

    [UPADTE] main.go uses new ShowUsage method

--- main.go
+++ main.go
@@ -70,15 +70,7 @@ func child() {
 
 	check(cmd.Run())
 
-	// Get memory usage after process exits
-	usage := cmd.ProcessState.SysUsage().(*syscall.Rusage)
-	fmt.Println("Memory Used: ", usage.Maxrss)
-	// Calutale Seconds in floating point
-	cpuTimeMicro := (usage.Stime.Sec*1000000 + usage.Stime.Usec) + (usage.Utime.Sec*1000000 + usage.Utime.Usec)
-
-	fmt.Printf("CPU time(usr+sys): %d Usec\n", cpuTimeMicro)
-	cpuTime := float64(cpuTimeMicro) / float64(1000000)
-	fmt.Printf("CPU time(usr+sys): %f Sec\n", cpuTime)
+	ShowUsage(cmd)
 }
 
 // Not using syscall.Setrlimit it's buggy ,using unix.Setrlimit
